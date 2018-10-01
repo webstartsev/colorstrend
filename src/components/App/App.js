9d9a5c80ef04ed156e582373b6dd6f25cced1d01
Sergey Startsev <sergey@webstartsev.ru>
2018-10-01 22:35:35 +0300

    add Header & Logo

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,10 +3,18 @@ import './App.css';
 
 import ColorList from '../ColorLIst/ColorList';
 
+import Header from '../Header/Header';
+import Logo from '../Logo/Logo';
+
+import colors from '../../data/colors';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header>
+          <Logo />
+        </Header>
         <ColorList className="color-list" colors={colors} />
       </div>
     );
---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Logo = () => {
+  return (
+    <div className="Logo">
+      <img className="Logo__img" src="" />
+    </div>
+  );
+};
+
+export default Logo;
