9ee5325fdd1fba4c3481c6daf81fb72820e33383
Sergey Startsev <sergey.starcev@imeretinka.ru>
2018-10-04 17:43:57 +0300

    AddColor шаблон

--- src/components/AddColor/AddColor.js
+++ src/components/AddColor/AddColor.js
@@ -2,7 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './AddColor.css';
 
-const AddColor = ({ onClick = f => f }) => <div className="AddColor" onClick={onClick} />;
+import icon from './plus.svg';
+
+const AddColor = ({ onClick = f => f }) => (
+  <div className="AddColor" onClick={onClick}>
+    <img src={icon} className="AddColor__icon" />
+    <span className="AddColor__text">Добавь свой цвет просто!</span>
+  </div>
+);
 
 AddColor.propTypes = {
   onClick: PropTypes.func
